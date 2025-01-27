ca1b02b5f7607d3b74828599f23492d41f54a97c
Marvin Xu <marvinxu99@hotmail.com>
2025-01-27 14:03:02 -0800

    ...

---
+++ MVVM_play/MVVM_play/Common/Category.cs
@@ -0,0 +1,21 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace MVVM_play.Common
+{
+
+    public class CategoryBase { }
+
+    public class Category : CategoryBase
+    {
+        public string Name { get; set; }
+        public string Tooltip { get; set; }
+        public Symbol Glyph { get; set; }
+    }
+
+    public class Separator : CategoryBase { }
+
+    public class Header : CategoryBase
+    {
+        public string Name { get; set; }
+    }
+}
--- MVVM_play/MVVM_play/MainWindow.xaml.cs
+++ MVVM_play/MVVM_play/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using MVVM_play.Common;
+using System.Collections.ObjectModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
+
 
 namespace MVVM_play;
 
@@ -10,9 +14,22 @@ namespace MVVM_play;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    public ObservableCollection<CategoryBase> Categories { get; set; }
+
     public MainWindow()
     {
         this.InitializeComponent();
+
+        Categories = new ObservableCollection<CategoryBase>
+        {
+            new Category { Name = "Category 1", Glyph = Symbol.Home, Tooltip = "This is category 1" },
+            new Category { Name = "Category 2", Glyph = Symbol.Keyboard, Tooltip = "This is category 2" },
+            new Category { Name = "Category 3", Glyph = Symbol.Library, Tooltip = "This is category 3" },
+            new Category { Name = "Category 4", Glyph = Symbol.Mail, Tooltip = "This is category 4" }
+        };
+
+        // Set DataContext on the root element (e.g., the Grid in XAML)
+        RootGrid.DataContext = this;
     }
 
 
