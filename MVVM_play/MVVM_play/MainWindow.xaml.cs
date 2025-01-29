319a1f31bd129d07fe12921b1feff6ee1d072814
Marvin Xu <marvinxu99@hotmail.com>
2025-01-28 17:05:01 -0800

    ...

--- MVVM_play/MVVM_play/MainWindow.xaml.cs
+++ MVVM_play/MVVM_play/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using MVVM_play.Common;
-using System.Collections.ObjectModel;
+using MVVM_play.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -14,29 +12,18 @@ namespace MVVM_play;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
-    public ObservableCollection<CategoryBase> Categories { get; set; }
+    public MainViewModel ViewModel { get; }
 
     public MainWindow()
     {
         this.InitializeComponent();
 
-        Categories = new ObservableCollection<CategoryBase>
-        {
-            new Category { Name = "Category 1", Glyph = Symbol.Home, Tooltip = "This is category 1",
-                Tag = "MVVM_play.Views.MountainPage" },
-            new Category { Name = "Category 2", Glyph = Symbol.Keyboard, Tooltip = "This is category 2",
-                Tag = "MVVM_play.Views.MountainPage"  },
-
-            new Separator(),
-
-            new Category { Name = "Category 3", Glyph = Symbol.Library, Tooltip = "This is category 3",
-                Tag = "MVVM_play.Views.AboutPage" },
-            new Category { Name = "Category 4", Glyph = Symbol.Mail, Tooltip = "This is category 4",
-                Tag = "MVVM_play.Views.AboutPage" }
-        };
+        // Initialize ViewModel
+        ViewModel = new MainViewModel();
 
         // Set DataContext on the root element (e.g., the Grid in XAML)
-        RootGrid.DataContext = this;
+        RootGrid.DataContext = ViewModel;
+
     }
 
 
---
+++ MVVM_play/MVVM_play/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml.Controls;
+using MVVM_play.Common;
+using System.Collections.ObjectModel;
+
+namespace MVVM_play.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<CategoryBase> _categories;
+
+    [ObservableProperty]
+    private ObservableCollection<CategoryBase> _footerMenuItems;
+
+    public MainViewModel()
+    {
+        Categories = new ObservableCollection<CategoryBase>
+        {
+            new Category { Name = "Category 1", Glyph = Symbol.Home, Tooltip = "This is category 1", Tag = "MVVM_play.Views.MountainPage" },
+            new Category { Name = "Category 2", Glyph = Symbol.Keyboard, Tooltip = "This is category 2", Tag = "MVVM_play.Views.MountainPage" },
+            new Separator(),
+            new Category { Name = "Category 3", Glyph = Symbol.Library, Tooltip = "This is category 3", Tag = "MVVM_play.Views.AboutPage" },
+            new Category { Name = "Category 4", Glyph = Symbol.Mail, Tooltip = "This is category 4", Tag = "MVVM_play.Views.AboutPage" }
+        };
+
+        FooterMenuItems = new ObservableCollection<CategoryBase>
+        {
+            new Category { Name = "Account", Glyph = Symbol.Contact, Tooltip = "Account Page", Tag = "MVVM_play.Views.AccountPage" },
+            new Category { Name = "About", Glyph = Symbol.Help, Tooltip = "About Page", Tag = "MVVM_play.Views.AboutPage" }
+        };
+    }
+}
