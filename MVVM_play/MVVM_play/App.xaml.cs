995500e34cff0ee12206e6a3baa84e59c4408003
Marvin Xu <marvinxu99@hotmail.com>
2025-01-28 21:11:19 -0800

    ...

--- MVVM_play/MVVM_play/App.xaml.cs
+++ MVVM_play/MVVM_play/App.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.UI.Xaml;
-using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,19 +26,19 @@ public partial class App : Application
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
         m_window = new MainWindow();
-        UnhandledException += App_UnhandledException;
+        //UnhandledException += App_UnhandledException;
         m_window.Activate();
     }
 
-    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
-    {
-        Debug.WriteLine($"Unhandled exception occurred: {e.Exception}");
-        Debug.WriteLine($"Message: {e.Message}");
-        Debug.WriteLine($"StackTrace: {e.Exception?.StackTrace}");
+    //private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    //{
+    //    Debug.WriteLine($"Unhandled exception occurred: {e.Exception}");
+    //    Debug.WriteLine($"Message: {e.Message}");
+    //    Debug.WriteLine($"StackTrace: {e.Exception?.StackTrace}");
 
-        // Prevent the app from crashing if appropriate
-        e.Handled = true;
-    }
+    //    // Prevent the app from crashing if appropriate
+    //    e.Handled = true;
+    //}
 
     private Window? m_window;
 }
