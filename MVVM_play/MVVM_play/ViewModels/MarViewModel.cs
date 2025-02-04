710d76dfdb114366f4ca8e2e45e235db6a949362
Marvin Xu <marvinxu99@hotmail.com>
2025-02-04 09:49:17 -0800

    ...

--- MVVM_play/MVVM_play/ViewModels/MarViewModel.cs
+++ MVVM_play/MVVM_play/ViewModels/MarViewModel.cs
@@ -1,9 +1,98 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace MVVM_play.ViewModels;
 
-internal class MarViewModel : ObservableObject
+internal class MarTask
 {
+    public string Name { get; set; }
+    public string TaskTime { get; set; }
+
+    public MarTask(string name, string taskTime)
+    {
+        Name = name;
+        TaskTime = taskTime;
+    }
+}
+
+internal class MarResult
+{
+    public string Name { get; set; }
+    public string ResultTime { get; set; }
+
+    public MarResult(string name, string resultTime)
+    {
+        Name = name;
+        ResultTime = resultTime;
+    }
+}
+
+internal class MarRowItem
+{
+    public string OrderName { get; set; }
+    public int OrderType { get; }
+    public List<MarTask>? Tasks { get; set; }
+    public List<MarResult>? Results { get; set; }
+
+    public MarRowItem(string orderName, int orderType)
+    {
+        OrderType = orderType;
+        OrderName = orderName;
+    }
+}
+
+internal partial class MarViewModel : ObservableObject
+{
+    private MarRowItem? _selectedMarRow;
+
+    public ObservableCollection<MarRowItem>? MarRows { get; set; }
+
+    public MarRowItem? SelectedMarRow
+    {
+        get => _selectedMarRow;
+        set => SetProperty(ref _selectedMarRow, value);
+    }
+
+
+    public RelayCommand RefreshMarCommand { get; }
+
+
+    public MarViewModel()
+    {
+        // Sample data
+        MarRows = new ObservableCollection<MarRowItem>
+        {
+            new("Order1", 1),
+            new("Order2", 1),
+            new("Order3", 2),
+            new("Order4", 2),
+            new("Order5", 3),
+            new("Order6", 3),
+            new("Order7", 3),
+        };
+
+        RefreshMarCommand = new RelayCommand(RefreshMar);
+    }
+
+    private void RefreshMar()
+    {
+        Debug.WriteLine($"RefreshMar()");
+    }
+
+    public void MarGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Debug.WriteLine("Selected Changed");
+    }
+
+    public void MarGrid_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        Debug.WriteLine("key pressed!");
+    }
 
 
 }
--- MVVM_play/MVVM_play/Views/MarPage.xaml.cs
+++ MVVM_play/MVVM_play/Views/MarPage.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml.Controls;
+using MVVM_play.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,8 +11,13 @@ namespace MVVM_play.Views;
 /// </summary>
 public sealed partial class MarPage : Page
 {
+    private MarViewModel ViewModel { get; }
+
     public MarPage()
     {
         this.InitializeComponent();
+
+        ViewModel = new MarViewModel();  // Create ViewModel instance
+        DataContext = ViewModel;            // Assign DataContext
     }
 }
