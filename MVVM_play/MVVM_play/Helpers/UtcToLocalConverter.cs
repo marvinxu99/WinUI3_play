ea85b6dc6f2b4679aee5e57d3f9b8b87877d0a70
Marvin Xu <marvinxu99@hotmail.com>
2025-02-07 22:20:12 -0800

    ...

---
+++ MVVM_play/MVVM_play/Helpers/UtcToLocalConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace MVVM_play.Helpers;
+
+internal partial class UtcToLocalConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is DateTime utcTime)
+        {
+            return utcTime.ToLocalTime().ToString("yyyy-MM-dd HH:mm");
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is string dateString && DateTime.TryParse(dateString, out DateTime localDateTime))
+        {
+            return localDateTime.ToUniversalTime();
+        }
+        return value;
+    }
+}
